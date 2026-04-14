c8064733e2f7cd9779d53ec69c79fac782185ce5
kaiqui <kaiqui82@gmail.com>
2026-04-14 08:55:39 -0300

    fix ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.3.6"
     id("io.ktor.plugin") version "3.1.0"
     application
     jacoco
